15240d61df2a2a9069ac9f9eb7f4309ecc1c9ad7
Bona Kim <bkim12@risd.edu>
2019-05-12 11:41:03 -0400

    Code Clean Up

--- sketch2.js
+++ sketch2.js
@@ -1,33 +1,26 @@
-let gifLength = 120;
+let gifLength = 260;
 let canvas;
 let video;
-var button;
-var snapshotting = false;
 
 
 
 function setup(){
   var p5Canvas = createCanvas(640,480);
+  background(random(255),random(255),random(255));
+  // background('#fae');
+
   video = createCapture(VIDEO);
   video.size(640,480);
   video.hide();
 
   canvas = p5Canvas.canvas;
-   capturer.start();
-
-  button = createButton('Record');
-  button.mousePressed(takesnap);
+  capturer.start();
 
 }
-
-function takesnap (){
-  snapshotting = true;
-}
-
-var counter = 0
 
 function draw(){
   image(video, 0, 0, 640, 480);
+
   if (frameCount < gifLength){
     capturer.capture(canvas);
   } else if (frameCount == gifLength){
